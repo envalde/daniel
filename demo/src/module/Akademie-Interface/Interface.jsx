8bab12537fc910109553ee1189614ce3617e2dc7
envalde <kohoutdanie@gmail.com>
2020-03-31 10:26:42 +0200

    final

---
+++ demo/src/module/Akademie-Interface/Interface.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Interface.css";
+import Navigation from "../../component/Navigation-no/Navigation";
+
+function Interface() {
+  return (
+    <>
+      <Navigation></Navigation>
+      <div className="Interface"></div>;
+    </>
+  );
+}
+
+export default Interface;
