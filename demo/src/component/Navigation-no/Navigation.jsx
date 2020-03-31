aaba502ddda708802d392c91881b02f5a8729e6a
envalde <kohoutdanie@gmail.com>
2020-03-31 09:57:25 +0200

    first

---
+++ demo/src/component/Navigation-no/Navigation.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import "./Navigation.css";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+function Navigation() {
+  return (
+    <>
+      <div className="Navigation">
+        <Navbar bg="" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">
+              <strong>Akademie</strong>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">Startseite</Nav.Link>
+                <Nav.Link href="#link">Kurse</Nav.Link>
+                <NavDropdown title="Mehr" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">
+                    Account Verwaltung
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Hilfe und Support
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form inline>
+                <FormControl
+                  type="text"
+                  placeholder="Search"
+                  className="mr-sm-2"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+      <div className="top-navigation">
+        <Container>
+          <Row>
+            <Col>Erfahren Sie wie Enval mit COVID-19 umgeht</Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default Navigation;
--- demo/src/index.js
+++ demo/src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
+import Navigation from "./component/Navigation-no/Navigation";
 import * as serviceWorker from "./serviceWorker";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Navigation />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
